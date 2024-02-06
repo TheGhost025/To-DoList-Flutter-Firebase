bbe914ce830b1ef00fbafa68799bd8c94cab4557
TheGhost025 <mazenkaluoby111@gmail.com>
2024-02-06 15:42:45 +0200

    add desgin for log in and sign up

---
+++ to_dolist/lib/logIn.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:to_dolist/signup.dart';
+
+class LogInPage extends StatefulWidget {
+  @override
+  LogInPageState createState() => LogInPageState();
+}
+
+class LogInPageState extends State<LogInPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Log In"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    label: Text("Email"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    icon: Icon(Icons.email)),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  label: Text("Password"),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)),
+                  icon: Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      isPasswordVisible
+                          // ignore: dead_code
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isPasswordVisible = !isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+                obscureText: !isPasswordVisible,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    print("Email: " + emailController.text);
+                    print("password: " + passwordController.text);
+                  },
+                  child: Text("Log In")),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("If you don't have account yet"),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage()));
+                      },
+                      child: Text("SignUp"))
+                ],
+              )
+            ],
+          ),
+          padding: EdgeInsets.all(16),
+        ),
+      ),
+    );
+  }
+}
